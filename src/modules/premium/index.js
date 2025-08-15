cf4a0121bddde78bb9aa15492cf520a46e0b5230
vantutr <tranvantu06042001@gmail.com>
2025-08-15 13:44:38 +0700

    sua premium trong package primium

--- src/modules/premium/index.js
+++ src/modules/premium/index.js
@@ -1,3 +1,4 @@
 // Premium module exports
-export { default as PremiumUpgrade } from './pages/PremiumUpgrade'
-export { default as TransactionHistory } from './pages/TransactionHistory'
+export { default as PremiumUpgrade } from "./pages/PremiumUpgrade";
+export { default as TransactionHistory } from "./pages/TransactionHistory";
+export { default as PaymentResult } from "./pages/PaymentResult";
